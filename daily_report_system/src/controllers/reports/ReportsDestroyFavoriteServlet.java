539573575864715e4dd1dd74ba103fd285297b0a
yuutakojima12 <58195587+yuutakojima12@users.noreply.github.com>
2020-02-24 04:32:27 +0900

    Add files via upload

--- daily_report_system/src/controllers/reports/ReportsDestroyFavoriteServlet.java
+++ daily_report_system/src/controllers/reports/ReportsDestroyFavoriteServlet.java
@@ -1,7 +1,6 @@
 package controllers.reports;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -10,72 +9,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Employee;
 import models.Favorite;
-import models.Report;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class ReportsFavoriteServlet
+ * Servlet implementation class ReportsDestroyFavoriteServlet
  */
-@WebServlet("/reports/favorite")
-public class ReportsFavoriteServlet extends HttpServlet {
+@WebServlet("/reports/destroy")
+public class ReportsDestroyFavoriteServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReportsFavoriteServlet() {
+    public ReportsDestroyFavoriteServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
 
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // TODO Auto-generated method stub
         String _token = (String)request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-
-
-            Favorite f = new Favorite();
-
-            // employee_id ログインidから //
-            f.setEmployee((Employee)request.getSession().getAttribute("login_employee"));
-
-            f.setReport((Report)request.getSession().getAttribute("report"));
-
-
-
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            f.setAdded_at(currentTime);
-
-
-
+            Favorite f = em.find(Favorite.class, (Integer)(request.getSession().getAttribute("favorite_id")));
 
             em.getTransaction().begin();
-            em.persist(f);
+            em.remove(f);
             em.getTransaction().commit();
-
             em.close();
-        }
 
+            request.getSession().removeAttribute("favorite_id");
 
-
-
-
-
-        response.sendRedirect(request.getContextPath() + "/reports/index");
-
+            response.sendRedirect(request.getContextPath() + "/reports/index");
     }
 
+}
 }
